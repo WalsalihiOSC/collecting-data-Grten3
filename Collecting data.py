8c16cf6e1598738d3c01716dc908dd6c75547835
Andy Tran <78826655+Grten3@users.noreply.github.com>
2022-04-15 18:22:52 +1200

    Adds basic functionality for "Enter data" button
    Stores user data into different lists

--- Collecting data.py
+++ Collecting data.py
@@ -1,5 +1,6 @@
 #Collecting data 
 
+
 from tkinter import *
 
 
@@ -7,22 +8,26 @@ class Data_GUI:
     def __init__(self,parent): 
         main = Frame(parent)
 
-        Processing.choice_list()
+       
+
+        
+        Processing.Lists()
        
         Label(main,text=" Collecting Personal Data").grid(row=0,column=0,padx=10,pady=30,sticky=E)
-
         Button(main,text="Show All").grid(row=0,column=1,pady=30,padx=10)
-
         Label(main,text="First Name: ").grid(row=1,column=0)
-        Entry(main).grid(row=1,column=1)
+        Data_GUI.name = Entry(main)
+        Data_GUI.name.grid(row=1,column=1)
         Label(main,text="Age: ").grid(row=2,column=0)
-        Entry(main).grid(row=2,column=1)
+        
+        Data_GUI.age = Entry(main)
+        Data_GUI.age.grid(row=2,column=1)
         Label(main,text="Do you have a mobile Phone?").grid(row=3,column=0,padx=5,pady=10)
         
 
         r = 3
         for i in(Processing.yeah_na):
-            Radiobutton(main, text=i, value=i).grid(column=1, row=r, sticky=W,pady=5)
+            Radiobutton(main, text=i, value=i, variable=Processing.selected_option).grid(column=1, row=r, sticky=W,pady=5)
             r += 1
 
         main.grid()
@@ -32,13 +37,36 @@ class Data_GUI:
 
         
 class Processing():
-    def choice_list():
+    def Lists():
+        Processing.name =[]
+        Processing.age = []
+        Processing.phone = [] 
+        Processing.selected_option = StringVar()
         Processing.yeah_na = ["Yes", "No"]
+
     
     def user_data():
-        name =[]
-        age=[]
-        phone=[]
+        print("****")
+
+
+        Processing.name.append(Data_GUI.name.get())
+        print(Processing.name)
+
+
+        Processing.age.append(Data_GUI.age.get())
+        print(Processing.age)
+
+        Processing.phone.append(Processing.selected_option.get())
+        print(Processing.phone)
+
+
+        
+        
+
+    
+        
+
+
 
 
     
