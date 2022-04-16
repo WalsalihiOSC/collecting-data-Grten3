5a5a111b6b750f855ecc37af16f9bdc9624a96ad
Andy Tran <78826655+Grten3@users.noreply.github.com>
2022-04-16 16:31:21 +1200

    working "previous" and "next" button

--- Collecting data.py
+++ Collecting data.py
@@ -35,8 +35,8 @@ class Data_GUI:
     
         Data_GUI.Enter_data = Button(Data_GUI.main_1,text="Enter Data",command=Processing.user_data)
         Data_GUI.Enter_data.grid(padx=5,pady=15,columnspan=2)
-        Data_GUI.Error_message = Label(Data_GUI.main_1,fg="red")
-        Data_GUI.Error_message.grid(padx=5,pady=5,columnspan=2)
+        Data_GUI.error_message = Label(Data_GUI.main_1,fg="red")
+        Data_GUI.error_message.grid(padx=5,pady=5,columnspan=2)
 
 #Show DATA Frame ********
 
@@ -48,17 +48,20 @@ class Data_GUI:
         Button(Data_GUI.main_2,text="Add new Person",command=Processing.enter).grid(row=0,column=1,pady=30,padx=10)
 
         Label(Data_GUI.main_2,text="First Name: ").grid(row=1,column=0,sticky=E)
-        Data_GUI.show_name = Label(Data_GUI.main_2, text="")
+        Data_GUI.show_name = Label(Data_GUI.main_2)
         Data_GUI.show_name.grid(row=1,column=1)
 
         Label(Data_GUI.main_2,text="Age: ").grid(row=2,column=0,sticky=E)
-        Data_GUI.show_age = Label(Data_GUI.main_2, text= "")
+        Data_GUI.show_age = Label(Data_GUI.main_2)
         Data_GUI.show_age.grid(row=2,column=1)
 
         Label(Data_GUI.main_2,text="Do you have a mobile Phone?").grid(row=3,column=0,padx=5,pady=10,columnspan=2)
 
         Button(Data_GUI.main_2,text="Previous",command=Processing.previous).grid(row=4,column=0,sticky=W,padx=10)
         Button(Data_GUI.main_2,text="Next",command=Processing.next).grid(row=4,column=1,sticky=E,padx=10)
+
+        Data_GUI.error_message_2 = Label(Data_GUI.main_2,fg="red")
+        Data_GUI.error_message_2.grid(columnspan=2,row=5,column=0,padx=10)
 
         
         
@@ -76,15 +79,14 @@ class Processing:
     
     def user_data():
         if len(Data_GUI.name.get()) == 0 or len(Data_GUI.age.get()) == 0 or len(Processing.selected_option.get()) == 0: 
-            Data_GUI.Error_message.configure(text = "*Please fill in all the info")
+            Data_GUI.error_message.configure(text = "*Please fill in all the info")
 
         else:
-            Data_GUI.Error_message.configure(text = "                              ")
+            Data_GUI.error_message.configure(text = "                              ")
 
             Processing.data.append(Data_GUI.name.get())
             Processing.data.append(Data_GUI.age.get())  
             Processing.data.append(Processing.selected_option.get())
-            print(Processing.data)
 
             Data_GUI.name.delete(0,END)
             Data_GUI.age.delete(0,END)
@@ -103,14 +105,35 @@ class Processing:
         Data_GUI.main_1.grid()
 
     def next():
-        Processing.i += 3
-        Data_GUI.show_name.configure(text=Processing.data[Processing.i])
-        Data_GUI.show_age.configure(text=Processing.data[Processing.i + 1])
+        try:
+            Processing.i += 3
+            Data_GUI.show_name.configure(text=Processing.data[Processing.i])
+            Data_GUI.show_age.configure(text=Processing.data[Processing.i + 1])
+            Data_GUI.error_message_2.configure(text="")
+
+        except:
+            Processing.i -= 3
+            Data_GUI.error_message_2.configure(text="You've reach the end of the list")
+
 
     def previous():
-        Processing.i -= 3
-        Data_GUI.show_name.configure(text=Processing.data[Processing.i])
-        Data_GUI.show_age.configure(text=Processing.data[Processing.i + 1])
+        if Processing.i == 0:
+                Processing.i += 3
+
+        else:
+            pass
+
+        try:
+            Processing.i -= 3
+            Data_GUI.show_name.configure(text=Processing.data[Processing.i])
+            Data_GUI.show_age.configure(text=Processing.data[Processing.i + 1])
+            Data_GUI.error_message_2.configure(text="")
+        
+                
+
+        except:
+            Processing.i += 3
+            Data_GUI.error_message_2.configure(text="You've reach the end of the list")
 
 
         
